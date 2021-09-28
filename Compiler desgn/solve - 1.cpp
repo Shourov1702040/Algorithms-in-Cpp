2ce8d6a46923747aaac57602188d36ee89235cf9
Md Touhid Islam <36205091+Shourov1702040@users.noreply.github.com>
2021-09-28 14:25:32 +0600

    Update solve - 1.cpp

--- Compiler desgn/solve - 1.cpp
+++ Compiler desgn/solve - 1.cpp
@@ -1,3 +1,13 @@
+/* Write a C/C++ program that reads text from a file and prints on the terminal each input 
+line, preceded by the line number. The output will look like -
+
+        1 This is the first trial line in the file,
+        2 and this is the second line.
+        
+Try the problem once using fgetc() function and once using fgets() function for reading 
+the input. Why is fread() not suitable for this purpose? */
+
+
 #include <bits/stdc++.h> 
 using namespace std;
 
