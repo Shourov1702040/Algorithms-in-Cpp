048f5f920f4f9aac67be2b6f6d6cf46d29f60485
Md Touhid Islam <36205091+Shourov1702040@users.noreply.github.com>
2021-10-19 07:02:03 -0700

    Add files via upload

---
+++ Operating system/Disk scheduling/04_LOOK.cpp
@@ -0,0 +1,104 @@
+// C++ program to demonstrate
+// LOOK Disk Scheduling algorithm
+int size = 8;
+#include <bits/stdc++.h>
+using namespace std;
+
+// Code by Vikram Chaurasia
+
+int disk_size = 200;
+
+void LOOK(int arr[], int head, string direction)
+{
+	int seek_count = 0;
+	int distance, cur_track;
+	vector<int> left, right;
+	vector<int> seek_sequence;
+
+	// appending values which are
+	// currently at left and right
+	// direction from the head.
+	for (int i = 0; i < size; i++) {
+		if (arr[i] < head)
+			left.push_back(arr[i]);
+		if (arr[i] > head)
+			right.push_back(arr[i]);
+	}
+
+	// sorting left and right vectors
+	// for servicing tracks in the
+	// correct sequence.
+	std::sort(left.begin(), left.end());
+	std::sort(right.begin(), right.end());
+
+	// run the while loop two times.
+	// one by one scanning right
+	// and left side of the head
+	int run = 2;
+	while (run--) {
+		if (direction == "left") {
+			for (int i = left.size() - 1; i >= 0; i--) {
+				cur_track = left[i];
+
+				// appending current track to seek sequence
+				seek_sequence.push_back(cur_track);
+
+				// calculate absolute distance
+				distance = abs(cur_track - head);
+
+				// increase the total count
+				seek_count += distance;
+
+				// accessed track is now the new head
+				head = cur_track;
+			}
+			// reversing the direction
+			direction = "right";
+		}
+		else if (direction == "right") {
+			for (int i = 0; i < int(right.size()); i++) {
+				cur_track = right[i];
+				// appending current track to seek sequence
+				seek_sequence.push_back(cur_track);
+
+				// calculate absolute distance
+				distance = abs(cur_track - head);
+
+				// increase the total count
+				seek_count += distance;
+
+				// accessed track is now new head
+				head = cur_track;
+			}
+			// reversing the direction
+			direction = "left";
+		}
+	}
+
+	cout << "Total number of seek operations = "
+		<< seek_count << endl;
+
+	cout << "Seek Sequence is" << endl;
+
+	for (int i = 0; i < int(seek_sequence.size()); i++) {
+		cout << seek_sequence[i] << endl;
+	}
+}
+
+// Driver code
+int main()
+{
+
+	// request array
+	int arr[size] = { 176, 79, 34, 60,
+					92, 11, 41, 114 };
+	int head = 50;
+	string direction = "right";
+
+	cout << "Initial position of head: "
+		<< head << endl;
+
+	LOOK(arr, head, direction);
+
+	return 0;
+}
